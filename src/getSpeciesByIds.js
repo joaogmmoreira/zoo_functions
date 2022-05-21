e904f451f62b5b3c29ef9644e6adca0cdf64bfa2
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-21 05:02:12 -0700

    requisitos 1 e 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  // const arr = [];
+  // for (let i = 0; i < species.length; i += 1) {
+  //   if (species[i].id === ids) {
+  //     arr.push(species[i]);
+  //     return arr;
+  //   }
+  // }
+  return species.filter((element) => ids.includes(element.id));
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
