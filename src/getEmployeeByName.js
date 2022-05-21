4d31dfcc5b7909cc24253d8a290298a478e3fa3b
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-21 05:19:25 -0700

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  return employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
+  // return employees.filter((element) => element.firstName === employeeName || element.lastName === employeeName);
 }
+
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
