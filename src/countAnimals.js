ab226b074591d1f7cbf885e8d6037fa3c576b4b3
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-23 09:59:19 -0700

    requisito 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animalsNames = species.map((element) => element.name);
+    const animalsQuantity = species.map((element) => element.residents.length);
+
+    // for (let i = 0; i < animalsNames; i += 1) {
+    //   animalObj[animalsNames[i]] = animalsQuantity[i];
+    //   console.log(animalObj);
+    // }
+
+    return animalObj;
+  }
+  return species.find((element) => element.name === animal.specie).residents.length;
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testa se HandlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('testa se handlerElephants recebendo um parâmetro count retorna 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Recebe um argumento que não é uma string e retorna mensagem Parâmetro inválido, é necessário uma string', () => {
+    expect(handlerElephants(27)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Recebe um argumento Undefined, e retorna undefined', () => {
+    expect(handlerElephants(undefined)).toBe(undefined);
+  });
+  it('Para o argumento location deve retornar a string NW', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+});
