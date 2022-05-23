d3affb4ab8399affba596acb8cc89bca3637d5f4
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-23 10:09:52 -0700

    corrigindo Lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,18 +2,18 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if (!animal) {
-    const animalsNames = species.map((element) => element.name);
-    const animalsQuantity = species.map((element) => element.residents.length);
+  // if (!animal) {
+  //   const animalsNames = species.map((element) => element.name);
+  //   const animalsQuantity = species.map((element) => element.residents.length);
 
-    // for (let i = 0; i < animalsNames; i += 1) {
-    //   animalObj[animalsNames[i]] = animalsQuantity[i];
-    //   console.log(animalObj);
-    // }
+  //   // for (let i = 0; i < animalsNames; i += 1) {
+  //   //   animalObj[animalsNames[i]] = animalsQuantity[i];
+  //   //   console.log(animalObj);
+  //   // }
 
-    return animalObj;
-  }
-  return species.find((element) => element.name === animal.specie).residents.length;
+  //   return animalObj;
+  // }
+  // return species.find((element) => element.name === animal.specie).residents.length;
 }
 
 console.log(countAnimals({ specie: 'penguins' }));
