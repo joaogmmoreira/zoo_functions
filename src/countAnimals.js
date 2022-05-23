1f9e164565f858c9e77837569570cf5db4c35d21
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-23 14:02:54 -0700

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,26 @@
-// const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // if (!animal) {
-  //   const animalsNames = species.map((element) => element.name);
-  //   const animalsQuantity = species.map((element) => element.residents.length);
-
-  //   // for (let i = 0; i < animalsNames; i += 1) {
-  //   //   animalObj[animalsNames[i]] = animalsQuantity[i];
-  //   //   console.log(animalObj);
-  //   // }
-
-  //   return animalObj;
-  // }
-  // return species.find((element) => element.name === animal.specie).residents.length;
+  if (!animal) {
+    const animalsObj = species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, []);
+    const sorted = Object.keys(animalsObj).sort().reduce((acc, curr) => {
+      acc[curr] = animalsObj[curr];
+      return acc;
+    }, {});
+    return sorted;
+  }
+  const maleOrFemale = species.find((element) => element.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex);
+  if (animal.sex) {
+    return maleOrFemale.length;
+  }
+  return species.find((element) => element.name === animal.specie).residents.length;
 }
-
-console.log(countAnimals({ specie: 'penguins' }));
+// console.log(countAnimals({ specie: 'penguins' }));
+// https://raullesteves.medium.com/javascript-entendendo-o-reduce-de-uma-vez-por-todas-c4cbaa16e380
 
 module.exports = countAnimals;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,27 @@
+const { species, hours } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animals = species.map((element) => element.name); // console.log(animals);
+  const daysOfTheWeek = Object.keys(hours); // console.log(daysOfTheWeek);
+  if (animals.includes(scheduleTarget)) {
+    const schedule1 = species.find((element) => element.name === scheduleTarget).availability;
+    return schedule1;
+  }
+  if (daysOfTheWeek.includes(scheduleTarget)) {
+    const schedule2 = species.filter((element) => element.availability.includes(scheduleTarget));
+    return schedule2;
+  }
+  const object = {
+    Tuesday: {
+      officeHour: Object.values(hours),
+      exhibition: species.filter((element1) => element1.availability
+        .includes('Tuesday')).map((element2) => element2.name),
+    },
+  };
+  return object;
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
