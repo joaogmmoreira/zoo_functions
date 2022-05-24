799e3d582ce128fcac4e3eb465e0bdb43aaf0e8b
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-24 14:27:54 -0700

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,21 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesCoverage(obj) {
+  const person = employees.filter((element) =>
+    element.firstName === obj.name || element.lastName === obj.name);
+  const animalsIdResponsibilities = person.map((element) => element.responsibleFor);
+  const animalsNamesResponsibilities = 
+  return animalsNamesResponsibilities;
+  // const result = person.map((element) => {
+  //   return {
+  //     id: element.id,
+  //     fullname: `${element.firstName} ${element.lastName}`,
+  //     species: 
+  //   };
+  // });
+  // return result;
 }
+console.log(getEmployeesCoverage({ name: 'Nigel' }));
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,20 +1,30 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
+//   const employee = data.employees.find((element) => element.id === id);
+//   const firstAnimal = employee.responsibleFor[0];
+//   const firstAnimalName = species.find((element) => element.id === firstAnimal).name;
+//   const oldestAnimalAge = species.find((element) => element.name === firstAnimalName)
+//     .residents.map((element2) => element2.age).sort((a, b) => b - a)[0];
+//   const oldestAnimalObj = species.find((element) => element.name === firstAnimalName).residents
+//     .filter((element2) => element2.age === oldestAnimalAge);
+//   const oldestAnimalArr = oldestAnimalObj.map((element) =>
+//     Object.values(element));
+//   return oldestAnimalArr;
+// }
 function getOldestFromFirstSpecies(id) {
   const employee = data.employees.find((element) => element.id === id);
-  const firstAnimal = employee.responsibleFor[0];
-  const firstAnimalName = species.find((element) => element.id === firstAnimal).name;
+  const firstAnimalId = employee.responsibleFor[0];
+  const firstAnimalName = species.find((element) => element.id === firstAnimalId).name;
   const oldestAnimalAge = species.find((element) => element.name === firstAnimalName)
     .residents.map((element2) => element2.age).sort((a, b) => b - a)[0];
   const oldestAnimalObj = species.find((element) => element.name === firstAnimalName).residents
     .filter((element2) => element2.age === oldestAnimalAge);
-  const oldestAnimalArr = oldestAnimalObj.map((element) =>
-    Object.values(element));
-  return oldestAnimalArr;
+  const oldestAnimalArr = oldestAnimalObj.map((element) => Object.values(element));
+  return oldestAnimalArr[0];
 }
+
 // https://stackoverflow.com/questions/34309090/convert-array-of-objects-into-array-of-properties
 
-console.log(getOldestFromFirstSpecies('56d43ba3-a5a7-40f6-8dd7-cbb05082383f'));
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
