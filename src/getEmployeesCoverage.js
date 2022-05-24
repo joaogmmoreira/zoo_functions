e4be45f90b39df0eef44c61089d8c6f7516a5413
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-24 14:33:33 -0700

    Corrigindo Lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -2,19 +2,19 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage(obj) {
-  const person = employees.filter((element) =>
-    element.firstName === obj.name || element.lastName === obj.name);
-  const animalsIdResponsibilities = person.map((element) => element.responsibleFor);
-  const animalsNamesResponsibilities = 
-  return animalsNamesResponsibilities;
-  // const result = person.map((element) => {
-  //   return {
-  //     id: element.id,
-  //     fullname: `${element.firstName} ${element.lastName}`,
-  //     species: 
-  //   };
-  // });
-  // return result;
+  // const person = employees.filter((element) =>
+  //   element.firstName === obj.name || element.lastName === obj.name);
+  // const animalsIdResponsibilities = person.map((element) => element.responsibleFor);
+  // // const animalsNamesResponsibilities = 
+  // // return animalsNamesResponsibilities;
+  // // const result = person.map((element) => {
+  // //   return {
+  // //     id: element.id,
+  // //     fullname: `${element.firstName} ${element.lastName}`,
+  // //     species: 
+  // //   };
+  // // });
+  // // return result;
 }
 console.log(getEmployeesCoverage({ name: 'Nigel' }));
 
