09d35d478db672c406ba6de6b6d2ef0efdd06f9a
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-25 12:24:26 -0700

    Requisito 15 e 16

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -31,6 +31,20 @@ describe('Testes da função getOpeningHours', () => {
   it('passando os argumentos Monday e 09:00-AM deve conter closed', () => {
     expect(getOpeningHours('Monday', '09:00-AM')).toContain('closed');
   });
+  it('passando os argumentos Tuesday e 09:00-PM retorna The zoo is closed', () => {
+    expect(getOpeningHours('Tuesday', '09:00-PM')).toBe('The zoo is closed');
+  });
+  it('passando os argumentos Monday e 09:00-AM retorna The zoo is closed', () => {
+    expect(getOpeningHours()).toEqual({
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    });
+  });
 });
 // https://jestjs.io/docs/using-matchers#exceptions
 // https://stackoverflow.com/questions/49027595/jest-test-that-exception-will-be-thrown-isnt-working
