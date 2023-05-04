3bfbe51becf857c16da8e52ce82ac7ec6c1268a0
João Moreira <jgmoreira1809@gmail.com>
2023-05-04 15:35:57 -0300

    Update countAnimals.js

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,6 +8,8 @@ function countAnimals(animal) {
       return acc;
     }, []);
 
+    // console.log(animalsObj)
+
     const sorted = Object.keys(animalsObj).sort().reduce((acc, curr) => {
       acc[curr] = animalsObj[curr];
       return acc;
@@ -22,10 +24,10 @@ function countAnimals(animal) {
   if (animal.sex) {
     return maleOrFemale.length;
   }
-  
+
   return species.find((element) => element.name === animal.specie).residents.length;
 }
-// console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'penguins' }));
 // https://raullesteves.medium.com/javascript-entendendo-o-reduce-de-uma-vez-por-todas-c4cbaa16e380
 
 module.exports = countAnimals;
